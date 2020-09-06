1d1236ffbc699165fe293bd0c7ced6695ee842c6
Jay Barria <jaybarria@gmail.com>
2020-09-06 19:54:23 +0800

    updates Added test driver

---
+++ src/main/kotlin/com/github/jairrab/calc/driver/TestDriver.kt
@@ -0,0 +1,38 @@
+/*
+ * Copyright (C) 2020 - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Created by Antonio Barria <jaybarria@gmail.com>
+ * Last modified 9/6/20 7:25 PM
+ */
+
+package com.github.jairrab.calc.driver
+
+import com.github.jairrab.calc.Calculator
+import com.github.jairrab.calc.CalculatorType
+
+fun main() {
+    val calculator = Calculator.getInstance(
+        calculatorType = CalculatorType.BASIC_MDAS,
+        initialNumber = 0.0,
+        listener = object : Calculator.Listener {
+            override fun onCalculatorUpdate(
+                key: String?,
+                entries: List<String>,
+                result: Double
+            ) {
+                //See runtime logs for output
+            }
+        }
+    )
+
+    calculator.pressOne()
+    calculator.pressTwo()
+    calculator.pressThree()
+    calculator.pressMultiply()
+    calculator.pressFour()
+    calculator.pressFive()
+    calculator.pressSix()
+    calculator.backSpace()
+    calculator.pressEquals()
+}
