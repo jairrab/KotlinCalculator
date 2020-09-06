000ae53f0e564e4d69af24bce82a79bcb3fe08f4
Jay Barria <jaybarria@gmail.com>
2020-09-06 13:18:56 +0800

    cleanups

--- Calculator/src/main/java/com/github/jairrab/calc/lib/utils/Logger.kt
+++
@@ -1,9 +0,0 @@
-/* This code is licensed under MIT license (see LICENSE.txt for details) */
-
-package com.github.jairrab.calc.lib.utils
-
-import java.util.logging.Logger
-
-object Logger {
-    val LOG: Logger = Logger.getLogger("CalculatorLog")
-}
--- Calculator/src/main/java/com/github/jairrab/calc/lib/utils/StringUtils.kt
+++
@@ -1,7 +0,0 @@
-/* This code is licensed under MIT license (see LICENSE.txt for details) */
-
-package com.github.jairrab.calc.lib.utils
-
-fun String.trimEndChar(): String {
-    return this.substring(0, this.length - 1)
-}
